c8062da554bb8aba1b47d37dbae5b5901050e7fe
Filip Radzikowski <filipr667@gmail.com>
2026-06-07 22:55:03 +0100

    Rename all src .js to .jsx, configure Vite to handle both

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "REACT_APP_");
 
   return {
-    plugins: [react()],
+    plugins: [react({ include: /\.(jsx|js)$/ })],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
